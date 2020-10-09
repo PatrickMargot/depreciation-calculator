fe2909661b56876bfcb3b39754401b3403ff5045
Patrick <margotpatrick02@gmail.com>
2020-10-09 12:52:27 +0200

    Updates

---
+++ precache-manifest.aef94ea22fd0446182a71903c2bc775a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0aeba4759a7b483fbb2a0e0aeeea547",
+    "url": "/PatrickMargot/depreciation-calculator/index.html"
+  },
+  {
+    "revision": "9927060caf5bacf9ea93",
+    "url": "/PatrickMargot/depreciation-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "213ecb01d97596c84767",
+    "url": "/PatrickMargot/depreciation-calculator/static/js/2.93ffaa38.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/PatrickMargot/depreciation-calculator/static/js/2.93ffaa38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9927060caf5bacf9ea93",
+    "url": "/PatrickMargot/depreciation-calculator/static/js/main.20c601af.chunk.js"
+  },
+  {
+    "revision": "7d20d4124de775b6796a",
+    "url": "/PatrickMargot/depreciation-calculator/static/js/runtime-main.f5b78d40.js"
+  }
+]);
