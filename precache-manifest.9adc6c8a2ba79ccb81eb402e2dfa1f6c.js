15d40302722eb6bf2e94ce57c244554746a23df8
Patrick <margotpatrick02@gmail.com>
2020-10-09 13:00:34 +0200

    Updates

---
+++ precache-manifest.9adc6c8a2ba79ccb81eb402e2dfa1f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb99dfe9073e5ee13efc39f9b950917f",
+    "url": "/depreciation-calculator/index.html"
+  },
+  {
+    "revision": "9927060caf5bacf9ea93",
+    "url": "/depreciation-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "213ecb01d97596c84767",
+    "url": "/depreciation-calculator/static/js/2.93ffaa38.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/depreciation-calculator/static/js/2.93ffaa38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9927060caf5bacf9ea93",
+    "url": "/depreciation-calculator/static/js/main.20c601af.chunk.js"
+  },
+  {
+    "revision": "6174c8eba99a4e7cb69e",
+    "url": "/depreciation-calculator/static/js/runtime-main.b6b579b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PatrickMargot/depreciation-calculator/precache-manifest.aef94ea22fd0446182a71903c2bc775a.js"
+  "/depreciation-calculator/precache-manifest.9adc6c8a2ba79ccb81eb402e2dfa1f6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PatrickMargot/depreciation-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/depreciation-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
